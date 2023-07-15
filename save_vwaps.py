8a708ed398c84a1fef1cd6bfbb3540ef2a0a9103
Ege Adıgüzel <16614043+eggadg@users.noreply.github.com>
2023-07-15 22:24:03 +0300

    uploading vwap saving script,updated EWMA notebood

--- save_vwaps.py
+++ save_vwaps.py
@@ -1,38 +1,26 @@
 import pandas as pd
-import os 
-from os import walk, mkdir
-from genericpath import exists
+import os
+from bamboo_utils import vwap, mkabsentdir
 import numpy as np
 
-day_paths = list()
-base_path="/media/ege/DATA/bamboo/RESAMPS/{}"
-for root, dirs, files in walk("/media/ege/DATA/bamboo/RESAMPS/"):
-    for dirname in dirs:
-        if dirname.startswith("2"):
-            print(dirname)
+base_path="/media/ege/DATA/bamboo/RESAMPS"
+path_vw = base_path+"/vwaps"
 
-def vwap(df):
-    s_i = df.trade_size.values              
-    p_i = df.ltp.values
-    return df.assign(vwap=(p_i * s_i).cumsum() / s_i.cumsum())
+mkabsentdir(path_vw)
 
-path_vw = "/media/ege/DATA/bamboo/RESAMPS/vwaps/"
-if not exists(path_vw):
-    mkdir(path_vw)
-"""    
-for day in day_paths:
-   mkdir(path_vw+day+"/")
-    
-for day in day_paths: 
-    print(day)
-    for root, dirs, files in walk(base_path.format(day)+"/"):
-        for file in files:
-            print(base_path.format(day)+"/"+file)
-            if "checkpoint" in file:
-                pass
-            elif file.endswith('.csv'):
-                df = pd.read_csv(base_path.format(day)+"/"+file)
-                df.trade_size = df.trade_size.abs()
-                df_vw = vwap(df)
-                df_vw.to_csv(path_vw+day+"/"+file)
-                """
+dirnames_list = os.listdir(base_path)
+dirnames_list = np.delete(dirnames_list,[4,5]).tolist()
+
+for dirname in dirnames_list:
+    mkabsentdir(os.path.join(path_vw,dirname))
+    for day_dir in os.listdir(os.path.join(base_path,dirname)):
+        print(day_dir)
+        mkabsentdir(os.path.join(path_vw,dirname,day_dir))
+        for root,dirs,files in os.walk(os.path.join(base_path,dirname,day_dir)):
+            for file in files:
+                if file.startswith("S") and file.endswith(".csv") and "checkpoint" not in file:
+                    df = pd.read_csv(os.path.join(base_path,dirname,day_dir,file))
+                    df.trade_size = df.trade_size.abs()
+                    df = df.dropna()
+                    df_vw = vwap(df)
+                    df_vw.to_csv(os.path.join(path_vw,dirname,day_dir,file))
